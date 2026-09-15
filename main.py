ab261c3b7899eeb40747eee8f6293334ee2eca7b
GerryT-git <tseng.gs@pg.com>
2026-09-15 17:49:47 -0400

    Update user_name in main.py.

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ def greet(name):
     return f"Hello, {name}! Welcome to Git and VS Code!"
 
 if __name__ == "__main__":
-    user_name = "World"
+    user_name = "Git Enthusiast" # Changed from "World"
     print(greet(user_name))
